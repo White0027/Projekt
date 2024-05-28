2a527c732959984f8c9458baeec67832661320ec
White0027 <tomas.urban2@unob.cz>
2024-05-28 15:21:36 +0200

    vypsani studentu v akreditaci

--- src/Components/Accreditation/AccreditationStudentsCard.jsx
+++ src/Components/Accreditation/AccreditationStudentsCard.jsx
@@ -1,27 +1,23 @@
-
-/* eslint-disable react/prop-types */
 import { CardCapsule } from '@hrbolek/uoisfrontend-shared/src'
-import { UserLink } from '../User/UserLink'
 
-
-const UserShort = ({user}) => {
+const UserShort = ({ fullname }) => {
     return (
         <>
-            <UserLink user={user} /> <br/>
+            {fullname} <br />
         </>
     )
 }
 
-export const AccreditationStudentsCard = ({accreditation, title="Studenti", valid=true}) => {
-    const fullname = accreditation?.students?.student?.fullname || []
-    const filtered = (valid===null)?fullname:fullname.filter(m => m?.valid === valid)
-    const mapped = filtered.map(m => m?.user)
+export const AccreditationStudentsCard = ({ accreditation, title = "Studenti", valid = true }) => {
+
+    const students = accreditation?.students || []
+    const fullnames = students.map(m => m?.student?.fullname)
+
     return (
         <CardCapsule title={title}>
-            {mapped.map(
-                u => <UserShort key={u.id} user={u} />
-            )}
+            {fullnames.map((fullname, index) => (
+                <UserShort key={index} fullname={fullname} />
+            ))}
         </CardCapsule>
-
     )
 }
