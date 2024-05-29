b7f68822547949ef662a7668bd278c69bb674cc7
Ondrabegy1 <Ondrabegy1@centrum.cz>
2024-05-29 09:17:00 +0200

    Oprava textu

--- src/Components/Accreditation/AccreditationSubjectCard.jsx
+++ src/Components/Accreditation/AccreditationSubjectCard.jsx
@@ -1,23 +1,3 @@
-// import { ProxyLink } from '@hrbolek/uoisfrontend-shared/src';
-// import Row from 'react-bootstrap/Row'
-// import { CardCapsule } from '@hrbolek/uoisfrontend-shared/src'
-
-// export const AccreditationSubjectCard = ({subject, children}) => {
-//     return (
-//         <CardCapsule title={"Předměty"}>
-//         <div>
-//             <Row>
-//             <ProxyLink to={"/subject/view/ce250a68-b095-11ed-9bd8-0242ac110002"}>Programování</ProxyLink>
-//             </Row>
-//             <Row>
-//             <ProxyLink to={"/subject/view/3ed497ea-ab40-4e94-a93e-9d9627497652" }>Databáze</ProxyLink>
-//             </Row>
-//         </div>
-//         </CardCapsule> 
-//     )
-// }
-
-import { AccreditationCreateLink, AccreditationLink } from "../Accreditation/AccreditationLink";
 import { formatDate } from "../Misc/FormatDate.jsx";
 import { CardCapsule } from "@hrbolek/uoisfrontend-shared/src";
 import { SortableTable } from "../Misc/SortableTable.jsx";
